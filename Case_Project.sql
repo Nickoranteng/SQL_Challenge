1071b9a0cd14b1513683eb44a67bffac2cf864e8
Nickoranteng <nickoranteng@gmail.com>
2020-11-22 21:17:18 -0500

    Employees from sales and Development

--- Case_Project.sql
+++ Case_Project.sql
@@ -109,3 +109,15 @@ on de.dept_no = d.dept_no
 where d.dept_name = 'Sales'
 
 
+/*7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.*/
+select e.emp_no
+	,e.last_name
+	,e.first_name
+	,d.dept_name  
+from employees as e
+LEFT join dept_emp as de
+on e.emp_no = de.emp_no
+left join departments as d
+on de.dept_no = d.dept_no
+where d.dept_name = 'Sales' or d.dept_name = 'Development'
+
