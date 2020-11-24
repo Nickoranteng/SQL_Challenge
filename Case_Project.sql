2244df665ba77d22345da93f75953d78d51d6ec2
Nickoranteng <nickoranteng@gmail.com>
2020-11-24 18:22:40 -0500

    case project queries

--- Case_Project.sql
+++ Case_Project.sql
@@ -8,7 +8,7 @@ select * from departments
 
 ----------------------------importing Employees Table from Csv-------------
 create table Employees (emp_no int primary key 
-						,emp_title varchar (20)
+						,emp_title varchar (20),constraint fk_emp_tit foreign key (emp_title) references Titles(title_id)
 						,birth_date date
 						,first_name varchar (30)
 						,last_name varchar(30)
@@ -19,8 +19,8 @@ copy employees from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-S
 select * from Employees;
 
 ---------------------------importing Dept_Emp Table from Csv-----------------
-create table Dept_Emp(emp_no int, constraint fk_emp foreign key (emp_no) references Employees (emp_no),
-					  Dept_no varchar (20), constraint fk_dept foreign key(dept_no) references Departments(Dept_no)
+create table Dept_Emp(emp_no int, constraint fk_emp foreign key (emp_no) references Employees (emp_no)
+					  ,Dept_no varchar (20), constraint fk_dept foreign key(dept_no) references Departments(Dept_no)
 					 );
  
 copy Dept_Emp from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\Part-2-Case-Assignment\data\dept_emp.csv' with CSV HEADER
@@ -28,8 +28,8 @@ copy Dept_Emp from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQ
 select * from Dept_Emp;
 
 --------------------------importing salaries Table from Csv--------------------
-create table salaries (emp_no int,constraint fk_emp_no foreign key (emp_no)references Employees (emp_no),
-					   salary money
+create table salaries (emp_no int,constraint fk_emp_no foreign key (emp_no)references Employees (emp_no)
+					   ,salary money
 					  );
 copy salaries from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\Part-2-Case-Assignment\data\salaries.csv' with CSV HEADER
 
@@ -37,8 +37,8 @@ select * from salaries;
 
 
 ---------------------------importing Titles Table from Csv------------------
-create table Titles (title_id varchar(30)primary key,
-					title varchar (40)
+create table Titles (title_id varchar(30)primary key
+					,title varchar (40)
 					);
 copy Titles from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\Part-2-Case-Assignment\data\Titles.csv' with CSV HEADER
 
@@ -49,7 +49,11 @@ copy Titles from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\
 
 
 
-
+select * from Departments;
+select * from Employees;
+select * from Dept_emp;
+select * from Salaries;
+select * from Titles;
 
 
 
@@ -61,18 +65,19 @@ select
 	,e.sex, s.salary 
 from employees as e
 left JOIN salaries as s
-on e.emp_no = s.emp_no
+on e.emp_no = s.emp_no;
 
 
 /*2. List first name, last name, and hire date for employees who were hired in 1986.*/
 select first_name
 	,last_name
 	,hire_date 
-from employees
+from Employees
 where hire_date >'1985-12-31'and hire_date <'1987-01-01'
-order by hire_date 
+order by hire_date; 
 
-/*3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.*/
+/*3. List the manager of each department with the following information: department number, department name, the manager's employee number, 
+last name, first name.*/
 
 
 /*4. List the department of each employee with the following information: employee number, last name, first name, and department name.*/
@@ -119,7 +124,8 @@ LEFT join dept_emp as de
 on e.emp_no = de.emp_no
 left join departments as d
 on de.dept_no = d.dept_no
-where d.dept_name = 'Sales' or d.dept_name = 'Development'
+where d.dept_name = 'Sales' or d.dept_name = 'Development';
+
 
 
 /*8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.*/
