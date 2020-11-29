efb818ed4350bf12abef8dd6ad200f48231b574a
Nickoranteng <nickoranteng@gmail.com>
2020-11-28 23:55:29 -0500

    EMPLOYEE sql Analysis

--- EMPLOYEESQL/EmployeeSQL.sql
+++ EMPLOYEESQL/EmployeeSQL.sql
@@ -4,7 +4,10 @@ CREATE TABLE Departments
 			 ,dept_name varchar(30)
 			)
 copy Departments from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\Part-2-Case-Assignment\data\departments.csv' with CSV HEADER;
+
 select * from departments
+
+
 
 ----------------------------importing Employees Table from Csv-------------
 create table Employees (emp_no int primary key 
@@ -16,7 +19,11 @@ create table Employees (emp_no int primary key
 						,hire_date Date not null
 					   );
 copy employees from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\Part-2-Case-Assignment\data\employees.csv' with CSV HEADER
+
 select * from Employees;
+
+
+
 
 ---------------------------importing Dept_Emp Table from Csv-----------------
 create table Dept_Emp(emp_no int, constraint fk_emp foreign key (emp_no) references Employees (emp_no)
@@ -28,6 +35,10 @@ copy Dept_Emp from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQ
 select * from Dept_Emp;
 
 ALTER TABLE Dept_Emp ADD CONSTRAINT PK_EMP_DEPT_NO PRIMARY KEY (emp_no,dept_no)
+
+
+
+
 ----------------------------
 create table dept_manager(dept_no varchar(20), constraint fk_dept_no foreign key(dept_no)references Departments (dept_no)
 						 ,emp_no int, constraint fk_emp_no foreign key (emp_no )references Employees(emp_no)
@@ -37,6 +48,10 @@ copy dept_manager from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\0
 select * from dept_manager
 
 ALTER TABLE dept_manager ADD CONSTRAINT PK_DEPT_EMP_NO PRIMARY KEY (dept_no,emp_no)
+
+
+
+
 --------------------------importing salaries Table from Csv--------------------
 create table salaries (emp_no int,constraint fk_emp_no foreign key (emp_no)references Employees (emp_no)
 					   ,salary money
@@ -44,6 +59,8 @@ create table salaries (emp_no int,constraint fk_emp_no foreign key (emp_no)refer
 copy salaries from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\Part-2-Case-Assignment\data\salaries.csv' with CSV HEADER
 
 select * from salaries;
+
+
 
 
 ---------------------------importing Titles Table from Csv------------------
@@ -54,18 +71,6 @@ copy Titles from 'C:\Users\nicko\gwu-arl-data-pt-09-2020-u-c\02-Homework\09-SQL\
 
 select * from titles;
 
-
-
-
-
-
-
-select * from Departments;
-select * from Employees;
-select * from Dept_emp;
-select * from dept_manager;
-select * from Salaries;
-select * from Titles;
 
 
 
@@ -90,6 +95,9 @@ where hire_date >'1985-12-31'
 and hire_date <'1987-01-01'
 order by hire_date; 
 
+
+
+
 /*3. List the manager of each department with the following information: department number, department name, the manager's employee number, 
 last name, first name.*/
 
@@ -108,6 +116,7 @@ on t.title_id = e.emp_title_id
 
 
 
+
 /*4. List the department of each employee with the following information: employee number, last name, first name, and department name.*/
 select e.emp_no
 	,e.last_name
@@ -120,6 +129,7 @@ left join departments as d
 on de.dept_no = d.dept_no;
 
 
+
 /*5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."*/
 
 select first_name
@@ -127,6 +137,8 @@ select first_name
 	,sex 
 from employees
 where first_name = 'Hercules' and last_name like  'B%'
+
+
 
 
 /*6. List all employees in the Sales department, including their employee number, last name, first name, and department name.*/
@@ -140,6 +152,8 @@ on e.emp_no = de.emp_no
 LEFT join departments as d
 on de.dept_no = d.dept_no
 where d.dept_name = 'Sales'
+
+
 
 
 /*7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.*/
@@ -156,11 +170,13 @@ where d.dept_name = 'Sales' or d.dept_name = 'Development';
 
 
 
+
 /*8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.*/
 select last_name, count(last_name) as frequency_count
 from employees
 group by last_name
 order by frequency_count desc;
+
 
 
 
